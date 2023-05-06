02ce64374f06ca6ef4b1ed11136cf94362983fba
jwaddell10 <jewaddell91@gmail.com>
2023-05-06 15:58:38 -0500

    discarded unnecessary code

--- index.js
+++ index.js
@@ -20,46 +20,30 @@ function playerMove () {
 
 }
 
-const Player = () => {
-const playerOneName = "Player One";
-const playerTwoName = "Player Two";
+const Player = (
+    playerOneName = "Player One",
+    playerTwoName = "Player Two") => {
+
+
+const players = [ 
+{
+    name: playerOneName,
+    token: 'X'
+}, 
+{
+    name: playerTwoName, 
+    token: 'O'
 }
+    ]
+    console.log(players);
+let activePlayer = players[0];
+}
+
 
 function Cell() {
     board.push('x');
 }
 
-
-const Players = (X, O) => {
-    const playerMove = () => console.log('is this running?')
-    const checkThis = () => console.log('X', 'O');
-    return {checkThis}
-}
-
-console.log(Players.checkThis);
-const personFactory = (name, age) => {
-    const sayHello = () => console.log('hello!');
-    return { name, age, sayHello };
-  };
-
-  const jeff = personFactory('jeff', 27);
-
-  jeff.sayHello(); // calls the function and logs 'hello!'
-
-console.log(jeff.name); // 'jeff'
-
-const calculator = (() => {
-    const add = (a, b) => a + b;
-    const sub = (a, b) => a - b;
-    const mul = (a, b) => a * b;
-    const div = (a, b) => a / b;
-    return {
-      add,
-      sub,
-      mul,
-      div,
-    };
-  })();
 //when cells are clicked, something can happen
   const cells = document.querySelectorAll('.box');
 
@@ -68,8 +52,3 @@ const calculator = (() => {
         console.log('is this running?')
     })
   })
-
-
-  console.log(calculator.add(3,5)); // 8
-  calculator.sub(6,2); // 4
-  calculator.mul(14,5534); // 77476
