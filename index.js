2dff8c4a721969578f8ab089bf9e5d9c21c8bc1e
jwaddell10 <jewaddell91@gmail.com>
2023-05-31 16:34:06 -0500

    created win conditions, displays winner to console

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 const gameBoard = (() => {
@@ -39,18 +40,52 @@ const gameController = (() => {
       activePlayer = playerOne;
     }
   }
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
+  function checkWinner() {
+    let roundWon = false;
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < 7; i++) {
+      const winningCondition = winConditions[i];
+      // eslint-disable-next-line prefer-const
+      let a = board[winningCondition[0]];
+      let b = board[winningCondition[1]];
+      let c = board[winningCondition[2]];
+
+      if (a === '' || b === '' || c === '') {
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+
+      if (a === b && b === c) {
+        roundWon = true;
+        console.log('winner');
+
+        break;
+      }
+    }
+    // eslint-disable-next-line no-const-assign, no-constant-condition, no-cond-assign
+  }
   function playMove() {
     const squares = document.querySelectorAll('.boxes').forEach((boxes) => boxes.addEventListener('click', (e) => {
-      switchPlayers();
+      checkWinner();
       if (boxes.innerHTML !== '') {
         return;
       }
+      switchPlayers();
       board[e.target.id - 1] = activePlayer.token;
       // eslint-disable-next-line no-param-reassign
       boxes.innerHTML = activePlayer.token;
       console.log(board);
     }));
-  }
-  playMove();
+  }playMove();
 })();
