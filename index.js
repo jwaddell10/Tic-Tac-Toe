434c69b77cbcc1cdc4705ddd5e4e3edc52bbaf66
jwaddell10 <jewaddell91@gmail.com>
2023-06-02 21:50:59 -0500

    fixed the logic to checkwinner

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
-const gameBoard = (() => {
-  const board = ['', '', '', '', '', '', '', '', ''];
-
-  const squares = document.querySelector('.squares');
-
-  // creates gameboard and displays to UI
-
-  // adds eventlistener to square to enable update to board
-  Array.from(squares.children).forEach((square, index) => {
-    square.addEventListener('click', () => {
-      board.innerHTML = `${index}`;
-      console.log(board);
-      console.log(this.data);
-    });
-  });
-  console.log(board);
-});
 const board = ['', '', '', '', '', '', '', '', ''];
 
 const createPlayer = (name, token) => ({ name, token });
 
-const gameController = (() => {
+const game = (() => {
   // eslint-disable-next-line max-len
   // function to switch turns, function to getactiveplayer, function to check winner, function to playround
   const playerOne = createPlayer('Player One', 'X');
@@ -31,7 +14,8 @@ const gameController = (() => {
   let activePlayer;
   console.log(activePlayer);
 
-  let gameOver;
+  let roundWon = false;
+  let remainingSpots = 9;
 
   function switchPlayers() {
     if (activePlayer === playerOne) {
@@ -52,7 +36,6 @@ const gameController = (() => {
   ];
 
   function checkWinner() {
-    let roundWon = false;
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < 7; i++) {
       const winningCondition = winConditions[i];
@@ -69,23 +52,57 @@ const gameController = (() => {
       if (a === b && b === c) {
         roundWon = true;
         console.log('winner');
+        const winnerText = document.querySelector('.displaytext');
+        const winText = document.createTextNode(`${activePlayer.name} wins!`);
+        winnerText.appendChild(winText);
 
         break;
       }
     }
     // eslint-disable-next-line no-const-assign, no-constant-condition, no-cond-assign
   }
-  function playMove() {
-    const squares = document.querySelectorAll('.boxes').forEach((boxes) => boxes.addEventListener('click', (e) => {
-      checkWinner();
+  function setupBoard() {
+    document.querySelectorAll('.boxes').forEach((boxes) => boxes.addEventListener('click', (e) => {
       if (boxes.innerHTML !== '') {
         return;
       }
+      console.log('checkWinner called');
       switchPlayers();
       board[e.target.id - 1] = activePlayer.token;
       // eslint-disable-next-line no-param-reassign
       boxes.innerHTML = activePlayer.token;
+      checkWinner();
       console.log(board);
     }));
-  }playMove();
+  }
+  function declareTie() {
+    console.log('its a tie!');
+  }
+  return {
+    setupBoard,
+    checkWinner,
+    winConditions,
+    switchPlayers,
+    roundWon,
+    activePlayer,
+    declareTie,
+    remainingSpots,
+  };
+})();
+
+const gameBoard = (() => {
+  // eslint-disable-next-line no-shadow
+  const board = ['', '', '', '', '', '', '', '', ''];
+
+  const squares = document.querySelector('.squares');
+
+  // creates gameboard and displays to UI
+  game.setupBoard();
+  console.log('Setup Board is done');
+  // adds eventlistener to square to enable update to board
+  Array.from(squares.children).forEach(() => {
+    // eslint-disable-next-line no-param-reassign
+    console.log(game.remainingSpots);
+    game.checkWinner();
+  });
 })();
